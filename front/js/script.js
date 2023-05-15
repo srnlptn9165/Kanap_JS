bb15b06458afcdf0eddf3d963dece07dd4c9e3fb
srnlptn9165 <sophiepetibon29@gmail.com>
2023-05-15 19:41:05 +0200

    affichage de la structure article

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,7 @@
 console.log("Bonjour dans le P5 - Essai affichage de données ! ");
 
  let art =document.getElementById('items');
- art.innerHTML = "<a><article><img><h3>productName</h3><p>productDescription</p></article></a>";
+ art.innerHTML = "<a><article><img><h3>nom de l'article</h3><p>Description de l'article</p></article></a>";
 
 // création d'un nouvel élément de type article , ajouté dans l'élément ayant pour id "items"
 let article = document.createElement("article");
@@ -9,19 +9,23 @@ document.getElementById("items").appendChild(article);
 
 let img = document.createElement("img");
 img.src = article.img;
+article.appendChild(img);
 let productName = document.createElement("h3");
 productName.innerText = article.productName;
 productName.classList.add("productName");
+article.appendChild(productName);
 let productDescription = document.createElement("p");
 productDescription.innerText = article.productDescription;
-productDescription.classList.add("productDescription")
+productDescription.classList.add("productDescription");
+article.appendChild(productDescription);
 
 // envoi d'un requête HTTP avec la méthode GET pour récupérer des données enregistrées à ce lien
 function readProduct() {
+    console.log("avant id fetch");
     fetch("http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926")
 
     //récupérer le résultat de la requête au format json
-    console.log("id fetch")
+    console.log("après id fetch")
     .then(function(res) {
         //vérifier que la requête s’est bien passée
         if (res.ok) {

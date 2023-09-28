3831a33191337cea8c300771a6c278d51f2bd89e
srnlptn9165 <sophiepetibon29@gmail.com>
2023-09-28 19:12:50 +0200

    create fctLocalStorage.js & nettoyage script.js et product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,20 +1,26 @@
-//---------------------------
-// Global Variables Definitions
-//---------------------------
+// VARIABLES
+const urlAPI = "http://localhost:3000/api/products";
 // Getting the product Id from the home page via URL
 let productId = new URL(location.href).searchParams.get("id");
-// -----------------------------------------
 
-// remplissage des champs à partir d'une clé associée à une valeur
+// Array Cart (Déclaration du panier sous forme de tableau)
+let cart = [];
+
+// création d'un objet : productSelected, pour capter les données sélectionnées dans l'écran
+let productSelected = {};
+productSelected._id = productId; // passage de l'identifiant de l'url, identifiant de l'objet consulté dans l'objet analysé
+
+//-------------------------------------------------------------------------------
+// FONCTIONS
+// fonction de remplissage des champs à partir de la clé "id" et de la valeur texte associée
 function fillField(id, text) {
   document.getElementById(id).innerText = text;
 }
 
-// Description des fonctions
-// récupération dans l'api product des différentes valeurs associées au numéro d'id
+// fonction de récupération dans l'api du détail de l'article
 function fillHTML() {
-  //   let productId = new URL(location.href).searchParams.get("id");
   fetch("http://localhost:3000/api/products/" + productId)
+    // fetch(`urlAPI/${productId}`)
     .then((resultat) => {
       if (resultat.ok) {
         return resultat.json();
@@ -22,9 +28,7 @@ function fillHTML() {
     })
 
     .then((product) => {
-      /* -----------------------------------------------------------
-                  --- Remplissage de la div Image avec class="item__img"  ---
-                  ----------------------------------------------------------- */
+      // --- Remplissage de la div Image avec class="item__img"  ---
       let img = document.createElement("img");
 
       img.src = product.imageUrl;
@@ -32,9 +36,7 @@ function fillHTML() {
 
       document.getElementsByClassName("item__img")[0].appendChild(img);
 
-      /* --------------------------------------------
-                  --- Remplissage des attributs du produit ---
-                  -------------------------------------------- */
+      // --- Remplissage des attributs du produit ---
       // Nom
       fillField("title", product.name);
 
@@ -44,9 +46,7 @@ function fillHTML() {
       // Description
       fillField("description", product.description);
 
-      /* --------------------------------
-                  --- Remplissage des couleurs ---
-                  -------------------------------- */
+      // --- Remplissage des couleurs ---
       let colors_section = document.getElementById("colors");
 
       for (let color of product.colors) {
@@ -62,61 +62,8 @@ function fillHTML() {
       console.log("The following error appeared: " + error);
     });
 }
-// Appel des fonctions
-// Affichage sur la page produit de l'élément choisi sur la page d'accueil à partir des données de l'API
-fillHTML();
 
-// Array Cart (Déclaration du tableau panier )
-let cart = [];
-
-// création d'un objet : productSelected, pour capter les données sélectionnées dans l'écran
-let productSelected = {};
-productSelected._id = productId; // passage de l'identifiant de l'url, identifiant de l'objet consulté dans l'objet analysé
-
-// prise en compte de la couleur choisie - EcouteAnalyse du clic
-let colorSelection = document.querySelector("#colors");
-colorSelection.addEventListener("input", (cs) => {
-  let colorSelected = cs.target.value; //récupération de la valeur de la cible de l'événement dans couleur
-  productSelected.color = colorSelected; // ajout de la couleur dans l'objet pour le panier
-});
-
-// Prise en compte de la quantité saisie - EcouteAnalyse du clic
-let quantitySelection = document.querySelector('input[id="quantity"]');
-quantitySelection.addEventListener("input", (itemQuantity) => {
-  productSelected.quantity = itemQuantity.target.value; // ajout de la quantity dans l'objet pour le panier
-  parseInt(productSelected.quantity);
-});
-
-// --------------------------------------------------
-// BAC : Action Click for ButtonAddCart (bouton "Ajout au panier")
-// --------------------------------------------------
-let addToCart = document.querySelector("#addToCart");
-
-addToCart.addEventListener("click", () => {
-  if (
-    parseInt(productSelected.quantity) < 1 ||
-    parseInt(productSelected.quantity) > 100 ||
-    productSelected.quantity === undefined
-  ) {
-    alert(
-      "pour ajouter un article au panier, une quantité non nulle doit être saisie"
-    );
-  } else {
-    if (productSelected.color === "" || productSelected.color === undefined) {
-      alert(
-        "pour ajouter un article au panier, une couleur doit être sélectionnée"
-      );
-    } else {
-      // ajout dans le panier du produit sélectionné à l'écran
-      addToTheCart(productSelected); // verif ok sans test
-      // changement du libellé du bouton à la fin de l'ajout
-      document.querySelector("#addToCart").textContent = "Ajout effectué"; // verif ok
-    }
-  }
-});
-
-// --------------------
-// ATTC : Fonction AddToTheCart pour ajout dans le panier d'un produit générique
+// Fonction d'ajout dans le panier d'un produit générique
 function addToTheCart(product) {
   let cart = getCart(); // récupération du panier : Vide ou non vide ?
 
@@ -143,36 +90,52 @@ function addToTheCart(product) {
   } else {
     cart.push(product);
     saveCart(cart);
-    console.log(cart);
   }
 }
 
-// --------------------
-// SC : fonction permettant d'enregistrer le panier désigné par le paramètre "cart" dans le localstorage
-// le local storage est une api qui permet de conserver des données, en enregistrant une valeur associée à une clé
-// le localstorage n’est pas capable d’enregistrer une donnée complexe comme un tableau ;
-// il faut transformer le tableau, donnée complexe, en une chaine de caractère avec JSON.stringify
-// --------------------
-function saveCart(cart) {
-  localStorage.setItem("cart", JSON.stringify(cart));
-}
-// --------------------
-// GC : fonction pour récupérer le panier vide ou avec des valeurs
-// GC : Si avec valeurs, récupération avec JSON.parse pour transformer la chaîne de caractère dans le tableau cartWithProduct
-function getCart() {
-  let cart = localStorage.getItem("cart");
-  // console.log(
-  //   "function getCart (récupération du panier vide ou existant) : ",
-  //   cart
-  // );
-  if (cart === null) {
-    // console.log("panier vide :");
-    return []; // renvoi du tableau vide
+// APPEL de la fonction d'affichage sur la page produit de l'élément choisi sur la page d'accueil à partir des données de l'API
+fillHTML();
+
+// --------------------------------------------------
+// ACTIONS
+// --------------------------------------------------
+
+// Prise en compte de la couleur choisie - EcouteAnalyse du clic
+let colorSelection = document.querySelector("#colors");
+colorSelection.addEventListener("input", (cs) => {
+  let colorSelected = cs.target.value; //récupération de la valeur de la cible de l'événement dans couleur
+  productSelected.color = colorSelected; // ajout de la couleur dans l'objet pour le panier
+});
+
+// Prise en compte de la quantité saisie - EcouteAnalyse du clic
+let quantitySelection = document.querySelector('input[id="quantity"]');
+quantitySelection.addEventListener("input", (itemQuantity) => {
+  productSelected.quantity = itemQuantity.target.value; // ajout de la quantity dans l'objet pour le panier
+  parseInt(productSelected.quantity);
+});
+
+// Click for ButtonAddCart (bouton "Ajout au panier")
+let addToCart = document.querySelector("#addToCart");
+
+addToCart.addEventListener("click", () => {
+  if (
+    parseInt(productSelected.quantity) < 1 ||
+    parseInt(productSelected.quantity) > 100 ||
+    productSelected.quantity === undefined
+  ) {
+    alert(
+      "pour ajouter un article au panier, une quantité non nulle doit être saisie"
+    );
   } else {
-    // console.log("panier renseigné :", cart);
-
-    return JSON.parse(cart); // renvoi du tableau renseigné
-    console.table(cart);
-    cartWithProduct = JSON.parse(cart);
+    if (productSelected.color === "" || productSelected.color === undefined) {
+      alert(
+        "pour ajouter un article au panier, une couleur doit être sélectionnée"
+      );
+    } else {
+      // ajout dans le panier du produit sélectionné à l'écran
+      addToTheCart(productSelected); // verif ok sans test
+      // changement du libellé du bouton à la fin de l'ajout
+      document.querySelector("#addToCart").textContent = "Ajout effectué"; // verif ok
+    }
   }
-}
+});
--- front/js/script.js
+++ front/js/script.js
@@ -1,25 +1,23 @@
-// console.log("1 - avant id fetch - envoi requête HTTP avec la méthode GET pour récupérer les données enregistrées sur ce lien");
-// envoi d'un requête HTTP avec la méthode GET pour récupérer des données enregistrées à ce lien
+// VARIABLES
+const urlAPI = "http://localhost:3000/api/products";
 
-function readArticle() { 
-  fetch("http://localhost:3000/api/products")
-  
+// FONCTION de récupération des données articles disponibles à partir de l'API
+
+function readArticle() {
+  fetch(urlAPI)
     //récupération du résultat de la requête au format json
     .then((resultat) => {
       if (resultat.ok) {
-        // console.log("2 - stockage des données dans un fichier json value ");
+        // stockage des données dans un fichier json
         return resultat.json();
       }
     })
 
-    .then((value) => {
+    .then((products) => {
       // traitement des données récupérées en json pour les afficher dans les bons champs
       // recuperation du resultat, en utilisant la boucle for of et en chargeant les éléments
-      // console.log("3 - valeurs du résultat : ");
-      console.table(value);
 
-      for(let product of value) {
-
+      for (let product of products) {
         // création d'un nouvel élément de type <a> ajouté dans l'élément ayant pour id "items"
         let a = document.createElement("a");
         document.getElementById("items").appendChild(a);
@@ -33,7 +31,7 @@ function readArticle() {
         img.src = product.imageUrl;
         img.alt = product.altTxt;
         article.appendChild(img);
-        
+
         let productName = document.createElement("h3");
         productName.classList.add("productName");
         productName.innerText = product.name;
@@ -43,7 +41,7 @@ function readArticle() {
         productDescription.classList.add("productDescription");
         productDescription.innerText = product.description;
         article.appendChild(productDescription);
-        
+
         let product_id = document.createElement("p");
         product_id.innerText = product._id;
 
@@ -53,25 +51,14 @@ function readArticle() {
         let productColors = document.createElement("table");
         productColors.innerText = product.colors;
 
-        // données articles affichées et/ou récupérées pour la page produit
-        // console.log("4 - boucle / données articles affichées et/ou récupérées pour la page produit / Article : ", product.name, " / id : ", product._id, " / prix : ", product.price, " / couleurs : ", product.colors);
-
         // modification de l'url pour passer l'id de l'article sélectionné
         a.href += "?id=" + product._id;
-        // console.log("5 - boucle / url avec id :" , a.href);
-
-        let liens = document.querySelectorAll("a");
-        // console.table("6 - boucle / liens <a> :", liens);
       }
     })
 
-    .catch(function(error) {
-      //une erreur est survenue
-      console.log("7 - l'erreur ", error, " est survenue");
-    })
+    .catch(function (error) {
+      console.log("l'erreur ", error, " est survenue");
+    });
 }
-// appel de la fonction créée
+// APPEL de la fonction créée
 readArticle();
-
-console.log("8 - FIN - Après appel de la fonction");
-
